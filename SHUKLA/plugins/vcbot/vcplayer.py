b7527840401414878ca43f2204a62684ec2631a6
It's RAJPUT <160611013+ITS-MY-EDIT@users.noreply.github.com>
2024-04-25 00:18:36 +0530

    Add files via upload

---
+++ SHUKLA/plugins/vcbot/vcplayer.py
@@ -0,0 +1,17 @@
+# Powered By // @ALL_SANATANI_BOT //
+
+__NAME__ = "Vᴄ Pʟᴀʏᴇʀ"
+__MENU__ = """
+**Set Stream Chat And Play From
+Anywhere You Want.**
+
+`.cset` - sᴇᴛ ʏᴏᴜʀ sᴛʀᴇᴀᴍ ᴄʜᴀᴛ.
+`.cjoin` - ᴊᴏɪɴ sᴛʀᴇᴀᴍ ᴄʜᴀᴛ ᴠᴄ.
+`.clve` - ʟᴇᴀᴠᴇ ғʀᴏᴍ sᴛʀᴇᴀᴍ ᴠᴄ.
+`.cply` - ᴘʟᴀʏ ʏᴏᴜʀ ᴀᴜᴅɪᴏ sᴏɴɢ.
+`.cvply` - ᴘʟᴀʏ ʏᴏᴜʀ ᴠɪᴅᴇᴏ sᴏɴɢ.
+`.cpse` - ᴘᴀᴜsᴇ ʀᴜɴɴɪɴɢ sᴛʀᴇᴀᴍ.
+`.crsm` - ʀᴇsᴜᴍᴇ ᴘᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ.
+`.cskp` - sᴋɪᴘ ᴛᴏ ɴᴇxᴛ sᴏɴɢ.
+`.cstp` - sᴛᴏᴘ sᴛʀᴇᴀᴍɪɴɢ ᴏɴ ᴠᴄ.
+"""
